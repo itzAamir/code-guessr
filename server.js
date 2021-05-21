2ed8a790be0594622749af3f2910ecb11f850e26
itzAamir <amirpc190320@gmail.com>
2021-05-21 14:59:28 +0530

    changed base directory

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(cors());
 app.use("/api", require("./route/route.js"));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../", "client", "build")));
+    app.use(express.static(path.join(__dirname, "client", "build")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
